9a19bd4d8b0f33ae3a2ec9ffdbcca60515889361
parag koche <paragkoche0@gmail.com>
2023-06-13 21:44:30 +0530

    ADD: ICON

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="https://app.boilerworldexpo.com/assets/Boiler_India.png"
+        ></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
