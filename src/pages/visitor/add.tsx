faff509abde39f2b932e3fa6970ee204c03373b4
parag koche <paragkoche0@gmail.com>
2023-06-13 11:07:45 +0530

    FIX: SET LABEL

--- src/pages/visitor/add.tsx
+++ src/pages/visitor/add.tsx
@@ -270,7 +270,7 @@ const Page = () => {
           <Stack spacing={3}>
             <Stack direction="row" justifyContent="space-between" spacing={4}>
               <Stack spacing={1}>
-                <Typography variant="h4">Exhibitor ADD</Typography>
+                <Typography variant="h4">Visitor ADD</Typography>
                 <Stack alignItems="center" direction="row" spacing={1}></Stack>
               </Stack>
               <div>
