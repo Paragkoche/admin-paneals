267bd45974ce91a714efab023bac3f385adda81f
parag koche <paragkoche0@gmail.com>
2023-06-15 18:32:00 +0530

    fix

--- src/pages/exhibitor/index.tsx
+++ src/pages/exhibitor/index.tsx
@@ -92,7 +92,7 @@ const Page = () => {
           <Stack spacing={3}>
             <Stack direction="row" justifyContent="space-between" spacing={4}>
               <Stack spacing={1}>
-                <Typography variant="h4">Exhibitor</Typography>
+                <Typography variant="h4">Exhibitors</Typography>
                 <Stack alignItems="center" direction="row" spacing={1}>
                   <Button
                     color="inherit"
