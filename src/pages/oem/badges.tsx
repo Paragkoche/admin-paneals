ad2f2be6af21defc22af8a3f38e986fcd6aba9af
parag koche <paragkoche0@gmail.com>
2023-07-03 23:50:17 +0530

    fix

--- src/pages/oem/badges.tsx
+++ src/pages/oem/badges.tsx
@@ -10,18 +10,26 @@ import ArrowDownOnSquareIcon from "@heroicons/react/24/solid/ArrowDownOnSquareIc
 import {
   Box,
   Button,
+  Card,
   CircularProgress,
   Container,
   Stack,
   SvgIcon,
+  Table,
+  TableBody,
   TableCell,
+  TableHead,
+  TablePagination,
   TableRow,
   Typography,
+  styled,
+  useTheme,
 } from "@mui/material";
 import { Head } from "next/document";
 import React from "react";
 import { useRouter } from "next/router";
 import Back from "@heroicons/react/24/solid/ArrowLeftIcon";
+import SimpleBar from "simplebar-react";
 export function applyPagination(
   documents: any[],
   page: number,
@@ -135,80 +143,80 @@ const Page = () => {
             </Stack>
           </Stack>
 
-          <Tabls
+          <Tables
             count={exhibitor.length}
             items={data}
             onPageChange={handlePageChange}
             onRowsPerPageChange={handleRowsPerPageChange}
             page={page}
             rowsPerPage={rowsPerPage}
-            child={() => {
-              let content = [];
+            // child={() => {
+            //   let content = [];
 
-              let data = exhibitor;
-              for (var i = 0; i < data.length; i++) {
-                if (data[i].exhibitor_mannings.length > 0) {
-                  let company_name =
-                    data[i].company_name != null ? data[i].company_name : "";
-                  let name =
-                    data[i].exhibitor_mannings[0].name != null
-                      ? data[i].exhibitor_mannings[0].name
-                      : "";
-                  let designation =
-                    data[i].exhibitor_mannings[0].designation != null
-                      ? data[i].exhibitor_mannings[0].designation
-                      : "";
-                  let email =
-                    data[i].exhibitor_mannings[0].email != null
-                      ? data[i].exhibitor_mannings[0].email
-                      : "";
-                  let mobile =
-                    data[i].exhibitor_mannings[0].mobile != null
-                      ? data[i].exhibitor_mannings[0].mobile
-                      : "";
-                  content.push([
-                    company_name.toUpperCase(),
-                    name.toUpperCase(),
-                    designation.toUpperCase(),
-                    email,
-                    mobile,
-                  ]);
-                  if (data[i].exhibitor_mannings.length > 1) {
-                    for (
-                      var j = 1;
-                      j < data[i].exhibitor_mannings.length;
-                      j++
-                    ) {
-                      content.push([
-                        "",
-                        data[i].exhibitor_mannings[j].name
-                          ? data[i].exhibitor_mannings[j].name.toUpperCase()
-                          : "",
-                        data[i].exhibitor_mannings[j].designation
-                          ? data[i].exhibitor_mannings[
-                              j
-                            ].designation.toUpperCase()
-                          : "",
-                        data[i].exhibitor_mannings[j].email
-                          ? data[i].exhibitor_mannings[j].email
-                          : "",
-                        data[i].exhibitor_mannings[j].mobile
-                          ? data[i].exhibitor_mannings[j].mobile
-                          : "",
-                      ]);
-                    }
-                  }
-                }
-              }
-              console.log(content);
-              return content.map((v) => (
-                <TableRow hover>
-                  {v.map((vv) => (
-                    <TableCell>{vv}</TableCell>
-                  ))}
-                </TableRow>
-              ));
-            }}
+            //   let data = exhibitor;
+            //   for (var i = 0; i < data.length; i++) {
+            //     if (data[i].exhibitor_mannings.length > 0) {
+            //       let company_name =
+            //         data[i].company_name != null ? data[i].company_name : "";
+            //       let name =
+            //         data[i].exhibitor_mannings[0].name != null
+            //           ? data[i].exhibitor_mannings[0].name
+            //           : "";
+            //       let designation =
+            //         data[i].exhibitor_mannings[0].designation != null
+            //           ? data[i].exhibitor_mannings[0].designation
+            //           : "";
+            //       let email =
+            //         data[i].exhibitor_mannings[0].email != null
+            //           ? data[i].exhibitor_mannings[0].email
+            //           : "";
+            //       let mobile =
+            //         data[i].exhibitor_mannings[0].mobile != null
+            //           ? data[i].exhibitor_mannings[0].mobile
+            //           : "";
+            //       content.push([
+            //         company_name.toUpperCase(),
+            //         name.toUpperCase(),
+            //         designation.toUpperCase(),
+            //         email,
+            //         mobile,
+            //       ]);
+            //       if (data[i].exhibitor_mannings.length > 1) {
+            //         for (
+            //           var j = 1;
+            //           j < data[i].exhibitor_mannings.length;
+            //           j++
+            //         ) {
+            //           content.push([
+            //             "",
+            //             data[i].exhibitor_mannings[j].name
+            //               ? data[i].exhibitor_mannings[j].name.toUpperCase()
+            //               : "",
+            //             data[i].exhibitor_mannings[j].designation
+            //               ? data[i].exhibitor_mannings[
+            //                   j
+            //                 ].designation.toUpperCase()
+            //               : "",
+            //             data[i].exhibitor_mannings[j].email
+            //               ? data[i].exhibitor_mannings[j].email
+            //               : "",
+            //             data[i].exhibitor_mannings[j].mobile
+            //               ? data[i].exhibitor_mannings[j].mobile
+            //               : "",
+            //           ]);
+            //         }
+            //       }
+            //     }
+            //   }
+            //   console.log(content);
+            //   return content.map((v) => (
+            //     <TableRow hover>
+            //       {v.map((vv) => (
+            //         <TableCell>{vv}</TableCell>
+            //       ))}
+            //     </TableRow>
+            //   ));
+            // }}
             tableHeadTitles={[
               "Company",
               "Exhibitor Name",
@@ -222,6 +230,79 @@ const Page = () => {
     </>
   ) : (
     <CircularProgress></CircularProgress>
+  );
+};
+const Scrollbar = styled(SimpleBar)``;
+const Tables = (props: any) => {
+  const theme = useTheme();
+  const {
+    count = 0,
+    items = [],
+    onPageChange = () => {},
+    onRowsPerPageChange,
+    page = 0,
+    rowsPerPage = 0,
+    selected = [],
+    refresh,
+  } = props;
+  console.log(items);
+  const [openS, setOpenS] = React.useState(false);
+  const [message, setMessage] = React.useState<string>("");
+  const [open, setOpen] = React.useState(false);
+  const [Dprops, setDProps] = React.useState<{
+    data?: {
+      title: string;
+      message: string;
+    };
+    action_button?: Array<{ fun: () => any; name: string }>;
+  }>({});
+
+  return (
+    <>
+      <Card>
+        <Scrollbar>
+          <Box sx={{ minWidth: 800 }}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  {[
+                    "Company",
+                    "Exhibitor Name",
+                    "Designation",
+                    "Email",
+                    "Mobile",
+                  ].map((v, i) => (
+                    <TableCell>{v}</TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {items.map((customer: any) => {
+                  return (
+                    <TableRow hover key={customer.id}>
+                      <TableCell>{customer.company_name}</TableCell>
+                      <TableCell>{customer.name}</TableCell>
+                      <TableCell>{customer.designation}</TableCell>
+                      <TableCell>{customer.email}</TableCell>
+                      <TableCell>{customer.mobile}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </Box>
+        </Scrollbar>
+        <TablePagination
+          component="div"
+          count={count}
+          onPageChange={onPageChange}
+          onRowsPerPageChange={onRowsPerPageChange}
+          page={page}
+          rowsPerPage={rowsPerPage}
+          rowsPerPageOptions={[5, 10, 25]}
+        />
+      </Card>
+    </>
   );
 };
 Page.getLayout = (page: any) => <Layout>{page}</Layout>;
